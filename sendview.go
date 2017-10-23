ecd6447de3747f17ca9916db09df8e2f3491981b
Riley Johnson <rileyjohnson99@hotmail.com>
2017-10-23 15:26:37 -0500

    Refactored picture sending

--- sendview.go
+++ sendview.go
@@ -1,48 +1,48 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"log"
+// 	"os"
 
-	"github.com/gorilla/websocket"
-	"github.com/loranbriggs/go-camera"
-)
+// 	"github.com/gorilla/websocket"
+// 	"github.com/loranbriggs/go-camera"
+// )
 
-type message struct {
-	Pi      bool   `json:"pi"`
-	Message string `json:"message"`
-	Pic     []byte `json:"pic"`
-}
+// type message struct {
+// 	Pi      bool   `json:"pi"`
+// 	Message string `json:"message"`
+// 	Pic     []byte `json:"pic"`
+// }
 
-func main() {
-	site := "ws://www.therileyjohnson.com/wsspy"
-	var dialer websocket.Dialer
-	//var m message
-	c := camera.New("pics/")
-	conn, _, err := dialer.Dial(site, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	s, err := c.Capture()
-	if err != nil {
-		fmt.Printf("Problem with camera\n%s\n", err)
-	}
-	wd, err := os.Getwd()
-	if err != nil {
-		fmt.Printf("Problem with getting working directory\n%s\n", err)
-	}
-	file, err := os.Open(wd + "/" + s)
-	if err != nil {
-		fmt.Printf("Problem with file\n%s\n", err)
-	}
-	fileInfo, _ := file.Stat()
-	size := fileInfo.Size()
-	sbytes := make([]byte, size)
-	buffer := bufio.NewReader(file)
-	_, err = buffer.Read(sbytes)
-	conn.WriteMessage(websocket.TextMessage, sbytes)
-	file.Close()
-	go os.Remove(s)
-}
+// func main() {
+// 	site := "ws://www.therileyjohnson.com/wsspy"
+// 	var dialer websocket.Dialer
+// 	//var m message
+// 	c := camera.New("pics/")
+// 	conn, _, err := dialer.Dial(site, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	s, err := c.Capture()
+// 	if err != nil {
+// 		fmt.Printf("Problem with camera\n%s\n", err)
+// 	}
+// 	wd, err := os.Getwd()
+// 	if err != nil {
+// 		fmt.Printf("Problem with getting working directory\n%s\n", err)
+// 	}
+// 	file, err := os.Open(wd + "/" + s)
+// 	if err != nil {
+// 		fmt.Printf("Problem with file\n%s\n", err)
+// 	}
+// 	fileInfo, _ := file.Stat()
+// 	size := fileInfo.Size()
+// 	sbytes := make([]byte, size)
+// 	buffer := bufio.NewReader(file)
+// 	_, err = buffer.Read(sbytes)
+// 	conn.WriteMessage(websocket.TextMessage, sbytes)
+// 	file.Close()
+// 	go os.Remove(s)
+// }
--- viewtiful.go
+++ viewtiful.go
@@ -5,15 +5,45 @@ import (
 	"fmt"
 	"log"
 	"os"
+    "mime/multipart"
 
 	"github.com/gorilla/websocket"
 	"github.com/loranbriggs/go-camera"
 )
 
+
+//From: https://matt.aimonetti.net/posts/2013/07/01/golang-multipart-file-upload-example/
+func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+  
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+  
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+  
+	req, err := http.NewRequest("POST", uri, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, err
+  }
+
 type message struct {
 	Pi      bool   `json:"pi"`
 	Message string `json:"message"`
-	Pic     []byte `json:"pic"`
 }
 
 func main() {
@@ -21,43 +51,71 @@ func main() {
 	var dialer websocket.Dialer
 	var m message
 	c := camera.New("pics/")
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Problem with getting working directory\n%s\n", err)
+		break
+	}
 	for {
 		conn, _, err := dialer.Dial(site, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		for {
+			var b bytes.Buffer
+			w := multipart.NewWriter(&b)
 			err = conn.ReadJSON(&m)
 			if err != nil {
 				fmt.Printf("JSON Read Error\n%s\n", err.Error())
 				break
 			}
+
 			s, err := c.Capture()
 			if err != nil {
 				fmt.Printf("Problem with camera\n%s\n", err)
 				break
 			}
-			wd, err := os.Getwd()
-			if err != nil {
-				fmt.Printf("Problem with getting working directory\n%s\n", err)
-				break
-			}
-			file, err := os.Open(wd + "/" + s)
-			if err != nil {
-				fmt.Printf("Problem with file\n%s\n", err)
-				break
-			}
-			fileInfo, _ := file.Stat()
-			size := fileInfo.Size()
-			sbytes := make([]byte, size)
-			buffer := bufio.NewReader(file)
-			_, err = buffer.Read(sbytes)
-			conn.WriteMessage(websocket.TextMessage, sbytes)
+			newfileUploadRequest("", nil, "pimg", wd + "/" + s)
+			// file, err := os.Open(wd + "/" + s)
+			// if err != nil {
+			// 	fmt.Printf("Problem with file\n%s\n", err)
+			// 	break
+			// }
+
+			// fw, err := w.CreateFormFile("image", wd + "/" + s)
+			// if err != nil {
+			// 	fmt.Printf("Problem with adding file to form\n%s\n", err)
+			// 	return 
+			// }
+
+			// if _, err = io.Copy(fw, f); err != nil {
+			// 	fmt.Printf("Problem with copying file to form\n%s\n", err)
+			// 	return
+			// }
+
+			// if fw, err = w.CreateFormField("pim"); err != nil {
+			// 	return
+			// }
+			// if _, err = fw.Write([]byte("PIM")); err != nil {
+			// 	return
+			// }
+
+			// w.Close()
+			// req, err := http.NewRequest("POST", url, &b)
+			// if err != nil {
+			// 	return 
+			// }
+			// fileInfo, _ := file.Stat()
+			// size := fileInfo.Size()
+			// sbytes := make([]byte, size)
+			// buffer := bufio.NewReader(file)
+			// _, err = buffer.Read(sbytes)
+			//conn.WriteMessage(websocket.TextMessage, sbytes)
 			/*if err := conn.WriteJSON(message{true, "", sbytes}); err != nil {
 				fmt.Println("JSON writing error", err)
 			}*/
-			file.Close()
-			go os.Remove(s)
+			go os.Remove(wd + "/" + s)
 		}
 	}
 }
